cfeb23a6ce9d0317c19210d2787980abdf825181
mcszen1 <mcszen@gmail.com>
2023-10-29 18:52:27 -0300

    Update Lines1.py

--- Lines1.py
+++ Lines1.py
@@ -4,27 +4,16 @@ import cv2
 import numpy as np
 
 
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
 
-    # Redimensiona a imagem para diminuir a carga de processamento
-    img = cv2.resize(img, (320, 240))
-
-    # Aplica o filtro Canny para detecção de bordas
-    edges = cv2.Canny(img, 100, 200)
-    edges_color = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-
-    return av.VideoFrame.from_ndarray(edges_color, format="bgr24")
 
 st.title("Detecção de Linhas em Tempo Real")
 
-# Iniciando o processamento de vídeo
-webrtc_streamer(key="example", 
-                video_frame_callback=callback,
-                media_stream_constraints={
-                    "video": {
-                        "width": {"ideal": 320},
-                        "height": {"ideal": 240},
-                        "frameRate": {"ideal": 15}
-                    }
-                })
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=callback)
